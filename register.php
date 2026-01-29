629af94c74aa94243ae73c70396011ccb31bbf57
Tommy <tommy.karotsch@laplateforme.io>
2026-01-29 14:36:05 +0100

    jour_1

--- register.php
+++ register.php
@@ -1,3 +1,16 @@
 <?php
 
 include 'config.php';
+include 'includes/header.php';
+
+?>
+<body>
+    <main>
+        <section>
+            <input type="text" name="login" placeholder="Login">
+            <input type="password" name="password" placeholder="Mot de passe">
+            <input type="submit" value="S'inscrire">
+        </section>
+    </main>
+</body>
+</html>
